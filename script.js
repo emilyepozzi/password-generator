c0df3b2b68a4da27e52914968b6f7ebd4df7a63d
Emily Pozzi <epozzi5@yahoo.com>
2021-02-28 15:38:00 -0600

    fixed bug in from console

--- script.js
+++ script.js
@@ -1,101 +1,97 @@
-// TODO declare your var at the top
-// Get references to the #generate element
+// Declare a variable and assign it a value
 var generateBtn = document.querySelector("#generate");
-
-// Defined arrays
+var passwordCharacters = [];
 var uppercaseLettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseLettersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialCharArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowercaseLettersArray = ["a", "b", "c",	"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharactersArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
 var passwordLength;
-var uppercaseLetter;
-var lowercaseLetter;
-var number;
-var specialChar;
-
-//TODO declare your functions 
-function passwordGenQs () {
-    passwordLength = prompt("How long would you like your password to be? Please pick a number bw 8 and 128.");
-    uppercaseLetter = confirm("Do you want upper case letters in your password?");
-    lowercaseLetter = confirm("Would you like lower case letters in your password?");
-    number = confirm("Would you like numbers in your password?");
-    specialChar = confirm("Would you like special characters in your password?");
-
-    if (passwordLength < 8 || passwordLength > 128 || passwordLength === null) {
-      alert("Your password must be between 8 and 128 characters.");
-      passwordGenQs();
-    }
-    //prompt user to select a number between 8 and 128. If they do not choose a number in that range, then call the function again(loop to the begining ) make sure to store nuymber length in variable. Return lenght 
-
-if (!uppercaseLetter && !lowercaseLetter && !number && !specialChar) {
-    alert("Your password must contain uppercase, lowercase, numbers and special characters. ");
-    passwordGenQs
-}
-    //prompt user with series of confirms asing if they want specific char. if they do not choose any call the function again. in the end makes sure we store choices in variables and return them
+var uppercaseLetters;
+var lowercaseLetters;
+var numbers;
+var specialCharacters;
 
 
-//objects . we need this key when we call this logic 
-var options = {
-passwordLength = passwordLength,
-uppercaseLetter = uppercaseLetter,
-lowercaseLetter = lowercaseLetter,
-number = number,
-specialChar = specialChar }
+//  Ask user for password criteria, store in variables, create an object, return object
+function passwordQuestions() {
+  resetApp();
+  passwordLength = prompt('How long would you like your password to be? Choose a number between 8 - 128.');
+  uppercaseLetters = confirm('Would you like uppercase letters?');
+  lowercaseLetters = confirm('Would you like lowercase letters?');
+  numbers = confirm('Would you like numbers?');
+  specialCharacters = confirm('Would you like special characters?');
 
-return options;
+  if (passwordLength < 8 || passwordLength > 128 || passwordLength === null) {
+    alert('Your selected password length has to be at least 8 characters and no more than 128.');
+    passwordQuestions();
+  }
 
-console.log(options);
+  if (!uppercaseLetters && !lowercaseLetters && !numbers && !specialCharacters) {
+      alert('Please select at least one character type to include in your password.');
+      passwordQuestions();
+  }
+
+  var options = {
+     passwordLength: passwordLength,
+     uppercaseLetters: uppercaseLetters,
+     lowercaseLetters: lowercaseLetters,
+     numbers: numbers,
+     specialCharacters: specialCharacters}
+
+  return options;
 }
 
 
+// if statements to add chosen arrays to passwordCharacters array, for loop to choose random letter for user preferences length
 function generatePassword() {
-    var length = passwordLength();
-    var options = passwordOptions();
-    var uppercaseLettersArray = [
-    
-    ];
-    var numberArray = []
-    var passwordGenArray = []:
-    
-    if (options.lowers ===true){
-    //.concatanation look into this 
-    }
-    
-    if (options.uppers === true) {
-    
-    }
-    
-    if (the rest of the variables) {
-    
-    }
-    
-    for (var i = 0; i < length; i+++){
-        //choose random letter of that passwrod gen array
-        var password + 
-    }
-    
-    //more logic here to determine what to include 
-    
-    //more logic here to loop to determine random char depending on length
-    
-    
-     //return randomly generated password 
-    }
-    
-    
-    
-    // Write password to the #password input
-    function writePassword() {
-        
-      var password = generatePassword();
-      // Selecting text area and writing password to text area 
-      var passwordText = document.querySelector("#password");
-    
-      passwordText.value = password;
-    
-    }
-    
-    
-    //TODO Add call functions and event listeners down here
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
+  var options = passwordQuestions();
+
+  if (options.uppercaseLetters) {
+    passwordCharacters = passwordCharacters.concat(uppercaseLettersArray);
+  }
+
+  if (options.lowercaseLetters) {
+    passwordCharacters = passwordCharacters.concat(lowercaseLettersArray);
+  }
+
+  if (options.numbers) {
+    passwordCharacters = passwordCharacters.concat(numbersArray);
+  }
+
+  if (options.specialCharacters) {
+    passwordCharacters = passwordCharacters.concat(specialCharactersArray);
+  }
+
+  var password = "";
+
+  for (var i = 0; i < options.passwordLength; i++) {
+      var randomCharacter = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+      password = password + randomCharacter;
+  }
+  
+  return password;
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword(); //gp => func 
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+
+// Function to reset passwordCharacters array
+function resetApp() {
+    passwordCharacters = [];
+    passwordLength;
+    uppercaseLetters;
+    lowercaseLetters;
+    numbers;
+    specialCharacters;
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
