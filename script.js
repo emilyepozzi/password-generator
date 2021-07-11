3431280662d627db0ac063ee7913f640691ab8ed
Emily Pozzi <epozzi5@yahoo.com>
2021-07-11 14:25:30 -0500

    updating javascript to final

--- script.js
+++ script.js
@@ -1,101 +1,97 @@
-// creating the button mechanism
-var createButton = document.querySelector("#create");
+// ref to generating buttons
+var generateBtn = document.querySelector("#generate");
 
-// Defined arrays
-var passwordCharacters = [];
-var uppercaseLettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseLettersArray = ["a", "b", "c",	"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharactersArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
-var passwordLength;
-var uppercaseLetters;
+// Defining arrays
+var passChars = [];
+var upperLetterArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerLetterArr = ["a", "b", "c",	"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var randomCharArr = [ "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "!", "#", "$", "%", "&", "(", ")", "*", "+"];
+var passLgth;
+var upperLetters;
 var lowercaseLetters;
 var numbers;
-var specialCharacters;
+var randomChars;
 
-//TODO declare your functions 
+// declaring functions 
 function passwordQuestions () {
     resetApp();
-passwordLength = prompt('How long is your password? Choose a number between 6-24.');
-uppercaseLetters = confirm('Would you like uppercase letters?');
-lowercaseLetters = confirm('Would you like lowercase letters?');
-numbers = confirm('Would you like numbers?');
-specialCharacters = confirm('Would you like special characters?');
+    passLgth = prompt('How many characters would you like your password to be? Choose a number between 6 and 24.');
+    upperLetters = confirm('Would you like to have uppercase letters?');
+lowercaseLetters = confirm('Would you like to have lowercase letters?');
+numbers = confirm('Would you like to have numbers?');
+randomChars = confirm('Would you like special characters?');
 
-  if (passwordLength < 6 || passwordLength > 24 || passwordLength === null) {
+  if (passLgth < 6 || passLgth > 24 || passLgth === null) {
     alert('Your selected password length has to be at least 6 characters and no more than 24.');
     passwordQuestions();
   }
-    //prompt user to select a number between 6 and 24. If they do not choose a number in that range, then call the function again to make sure to store number length in variable. Return the length of password chosen. 
 
-    if (!uppercaseLetters && !lowercaseLetters && !numbers && !specialCharacters) {
-        alert('Please select at least one character type to include in your password.');
+    if (!upperLetters && !lowercaseLetters && !numbers && !randomChars) {
+        alert('Select at least one character type to use in your password.');
         passwordQuestions();
     }
-    //prompt user with series of confirms asing if they want specific char. if they do not choose any call the function again. in the end makes sure we store choices in variables and return them
+    //Prompt user with set of q`s 
 
-
-//objects . we need this key when we call this logic 
+//options. need this when listing objects
 var options = {
-    passwordLength: passwordLength,
-    uppercaseLetters: uppercaseLetters,
+  passLgth: passLgth,
+  upperLetters: upperLetters,
     lowercaseLetters: lowercaseLetters,
     numbers: numbers,
-    specialCharacters: specialCharacters}
+    randomChars: randomChars}
 
  return options;
 }
 
 
 
-function createPassword() {
+function generatePassword() {
     var options = passwordQuestions();
   
-    if (options.uppercaseLetters) {
-      passwordCharacters = passwordCharacters.concat(uppercaseLettersArray);
+    if (options.upperLetters) {
+      passChars = passChars.concat(upperLetterArr);
     }
   
     if (options.lowercaseLetters) {
-      passwordCharacters = passwordCharacters.concat(lowercaseLettersArray);
+      passChars = passChars.concat(lowerLetterArr);
     }
   
     if (options.numbers) {
-      passwordCharacters = passwordCharacters.concat(numbersArray);
+      passChars = passChars.concat(numberArr);
     }
   
-    if (options.specialCharacters) {
-      passwordCharacters = passwordCharacters.concat(specialCharactersArray);
+    if (options.randomChars) {
+      passChars = passChars.concat(randomCharArr);
     }
   
     var password = "";
-    for (var i = 0; i < options.passwordLength; i++) {
-        var randomCharacter = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    for (var i = 0; i < options.passLgth; i++) {
+        var randomCharacter = passChars[Math.floor(Math.random() * passChars.length)];
         password = password + randomCharacter;
     }
-    //choose random letter of that password 
+    //choose random letter of that password to generate the array
   
-     //randomly created password 
+     //return randomly generated password 
     return password;
-}
-      
+}      
+    // Write password to the pass input
     function writePassword() {
-        var password = createPassword(); //gp => func 
+        var password = generatePassword();
         var passwordText = document.querySelector("#password");
       
         passwordText.value = password;
       }
-      // Selecting text area and writing password to text area 
 
-
-    // function to reset passwordCharacters array
+    // function to reset passChars array
     function resetApp() {
-        passwordCharacters = [];
-        passwordLength;
-        uppercaseLetters;
+      passChars = [];
+        passLgth;
+        upperLetters;
         lowercaseLetters;
         numbers;
-        specialCharacters;
+        randomChars;
     }
     
-    // Add event listener to create button
-    createButton.addEventListener("click", writePassword);
+    // Event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
